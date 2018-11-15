1f7bb0a74bf451e83efb4385675c32ecdda0ec61
Yathindra Kodithuwakku <yathindrarawya123@gmail.com>
2018-11-15 12:00:47 +0530

    completed basic functionality

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,6 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.swing.*;
 import java.awt.*;
 
@@ -16,11 +19,11 @@ public class Calculator extends JFrame{
     private JButton btnZero;
 
     // buttons for operations
-    private JButton btnoPlus;
+    private JButton btnPlus;
     private JButton btnMinus;
     private JButton btnMultiply;
-    private JButton btnoDivide;
-    private JButton btnoEqual;
+    private JButton btnDivide;
+    private JButton btnEqual;
 
     //  buttons for additional functionality
     private JButton btnAC;
@@ -31,34 +34,16 @@ public class Calculator extends JFrame{
     // display
     private JTextField display;
 
-
-    static Color clr;
+    private Color clr;
     private Color rightBtnColor;
 
-    static StringBuffer br = new StringBuffer();
-    static StringBuffer br1 = new StringBuffer();
-
-
-    static int number1;
-    static int number2;
-    static int result=0;
-    static String no1;
-    static String no2;
-    static JButton bx;
-    static String ans;
-
     // fonts
-    private Font f;
-    private Font f1;
-    private Font f2;
-    static String str;
-    static String newstr;
+    private Font btnFont;
+    private Font displayFont;
 
-    static float  n2;
+    static float num2;
 
-    static int len;
-    static int newlen;
-    static int t1len;
+    private Color overrallClr;
 
 
     public static void main(String args[])
@@ -68,11 +53,11 @@ public class Calculator extends JFrame{
 
     public Calculator() {
         super("Calc");
+        // JFrame properties
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //JFrame
         setBounds(100,50,296,419);
         setResizable(false);
-
 
         showDisplay();
         showButtons();
@@ -80,25 +65,23 @@ public class Calculator extends JFrame{
 
     private void showDisplay() {
 
+        // init display
         display = new JTextField("0");
 
-        // fonts
-        f = new Font("Century",Font.PLAIN,20);
-        f1 = new Font("Times New Roman",Font.PLAIN,45);
-        f2 = new Font("Times New Roman",Font.PLAIN,25);
+        // init fonts
+        btnFont = new Font("Century",Font.PLAIN,20);
+
+        displayFont = new Font("Times New Roman",Font.PLAIN,45);
 
         display.setBounds(1,0,289,100);
         display.setDisabledTextColor(Color.ORANGE);
-        display.setFont(f1);
+        display.setFont(displayFont);
+        display.setEditable(false);
 
-
-        Container c = getContentPane();
-        c.setLayout(null);
+        setLayout(null);
 
         setBackground(Color.gray);
-        c.setBackground(Color.gray);
 
-//        display.setBackground(clr);
         display.setForeground(Color.black);
     }
 
@@ -116,11 +99,11 @@ public class Calculator extends JFrame{
         btnNine = new JButton("9");
         btnZero = new JButton("0");
 
-        btnoDivide = new JButton(" / ");
+        btnDivide = new JButton(" / ");
         btnMultiply = new JButton(" * ");
         btnMinus = new JButton(" - ");
-        btnoPlus = new JButton(" + ");
-        btnoEqual = new JButton("=");
+        btnPlus = new JButton(" + ");
+        btnEqual = new JButton("=");
         btnAC = new JButton("C");
 
         btnPlusMinus = new JButton("+/-");
@@ -137,105 +120,328 @@ public class Calculator extends JFrame{
         add(btnEight);
         add(btnNine);
         add(btnZero);
-        add(btnoEqual);
-        add(btnoPlus);
+        add(btnEqual);
+        add(btnPlus);
         add(btnMinus);
         add(btnMultiply);
-        add(btnoDivide);
+        add(btnDivide);
         add(display);
         add(btnAC);
         add(btnPlusMinus);
         add(btnDecimal);
         add(btnModulus);
 
+        btnAC.setBounds(1,101,73,59);
+        btnPlusMinus.setBounds(73,101,73,59);
+        btnModulus.setBounds(145,101,73,59);
 
+        btnFour.setBounds(1,216,74,60);
+        btnFive.setBounds(73,216,73,59);
+        btnSix.setBounds(145,216,73,59);
 
+        btnSeven.setBounds(1,158,73,59);
+        btnEight.setBounds(73,158,73,59);
+        btnNine.setBounds(145,158,73,59);
 
-        ImageIcon icon = new ImageIcon("cal.png");
-        setIconImage(icon.getImage());
+        btnOne.setBounds(1,274,73,59);
+        btnThree.setBounds(145,274,73,59);
 
+        btnZero.setBounds(1,332,145,59);
+        btnDecimal.setBounds(145,332,73,59);
+        btnDivide.setBounds(217,100,73,59);
 
+        btnTwo.setBounds(73,274,73,59);
 
-        btnAC.setBounds(1,100,72,58);
-        btnPlusMinus.setBounds(73,100,72,58);
-        btnModulus.setBounds(145,100,72,58);
+        btnMultiply.setBounds(217,158,73,59);
+        btnMinus.setBounds(217,216,73,59);
+        btnPlus.setBounds(217,274,73,59);
+        btnEqual.setBounds(217,332,73,59);
 
-        btnFour.setBounds(1,216,72,58);
-        btnFive.setBounds(73,216,72,58);
-        btnSix.setBounds(145,216,72,58);
-
-        btnSeven.setBounds(1,158,72,58);
-        btnEight.setBounds(73,158,72,58);
-        btnNine.setBounds(145,158,72,58);
-
-        btnOne.setBounds(1,274,72,58);
-        btnThree.setBounds(145,274,72,58);
-
-        btnZero.setBounds(1,332,144,58);
-        btnDecimal.setBounds(145,332,72,58);
-        btnoDivide.setBounds(217,100,72,58);
-
-        btnTwo.setBounds(73,274,72,58);
-
-        btnMultiply.setBounds(217,158,72,58);
-        btnMinus.setBounds(217,216,72,58);
-        btnoPlus.setBounds(217,274,72,58);
-        btnoEqual.setBounds(217,332,72,58);
-
-        Color col = new Color(224, 224, 224);
+        overrallClr = new Color(224, 224, 224);
         rightBtnColor = new Color(255, 153, 51);
 
-        btnOne.setBackground(col);
-        btnTwo.setBackground(col);
-        btnThree.setBackground(col);
-        btnFour.setBackground(col);
-        btnFive.setBackground(col);
-        btnSix.setBackground(col);
-        btnSeven.setBackground(col);
-        btnEight.setBackground(col);
-        btnNine.setBackground(col);
-        btnZero.setBackground(col);
+        btnOne.setBackground(overrallClr);
+        btnTwo.setBackground(overrallClr);
+        btnThree.setBackground(overrallClr);
+        btnFour.setBackground(overrallClr);
+        btnFive.setBackground(overrallClr);
+        btnSix.setBackground(overrallClr);
+        btnSeven.setBackground(overrallClr);
+        btnEight.setBackground(overrallClr);
+        btnNine.setBackground(overrallClr);
+        btnZero.setBackground(overrallClr);
 
-        btnModulus.setBackground(col);
-        btnAC.setBackground(col);
+        btnModulus.setBackground(overrallClr);
+        btnAC.setBackground(overrallClr);
 
-        btnoPlus.setBackground(rightBtnColor);
+        btnPlus.setBackground(rightBtnColor);
         btnMinus.setBackground(rightBtnColor);
         btnMultiply.setBackground(rightBtnColor);
-        btnoDivide.setBackground(rightBtnColor);
+        btnDivide.setBackground(rightBtnColor);
 
-        btnPlusMinus.setBackground(col);
-        btnDecimal.setBackground(col);
-        btnoEqual.setBackground(rightBtnColor);
+        btnPlusMinus.setBackground(overrallClr);
+        btnDecimal.setBackground(overrallClr);
+        btnEqual.setBackground(rightBtnColor);
 
         display.setHorizontalAlignment(display.RIGHT);
 
-        btnOne.setFont(f);
-        btnTwo.setFont(f);
-        btnThree.setFont(f);
-        btnFour.setFont(f);
-        btnFive.setFont(f);
-        btnSix.setFont(f);
-        btnSeven.setFont(f);
-        btnEight.setFont(f);
-        btnNine.setFont(f);
-        btnZero.setFont(f);
-        btnoPlus.setFont(f);
-        btnMinus.setFont(f);
-        btnMultiply.setFont(f);
-        btnoDivide.setFont(f);
-        btnoEqual.setFont(f);
+        btnOne.setFont(btnFont);
+        btnTwo.setFont(btnFont);
+        btnThree.setFont(btnFont);
+        btnFour.setFont(btnFont);
+        btnFive.setFont(btnFont);
+        btnSix.setFont(btnFont);
+        btnSeven.setFont(btnFont);
+        btnEight.setFont(btnFont);
+        btnNine.setFont(btnFont);
+        btnZero.setFont(btnFont);
+        btnPlus.setFont(btnFont);
+        btnMinus.setFont(btnFont);
+        btnMultiply.setFont(btnFont);
+        btnDivide.setFont(btnFont);
+        btnEqual.setFont(btnFont);
 
-        btnPlusMinus.setFont(f);
-        btnDecimal.setFont(f);
-        btnModulus.setFont(f);
+        btnPlusMinus.setFont(btnFont);
+        btnDecimal.setFont(btnFont);
+        btnModulus.setFont(btnFont);
 
-        btnAC.setFont(f);
+        btnAC.setFont(btnFont);
 
-        btnoPlus.setForeground(col);
-        btnMinus.setForeground(col);
-        btnMultiply.setForeground(col);
-        btnoDivide.setForeground(col);
-        btnoEqual.setForeground(col);
+        btnPlus.setForeground(overrallClr);
+        btnMinus.setForeground(overrallClr);
+        btnMultiply.setForeground(overrallClr);
+        btnDivide.setForeground(overrallClr);
+        btnEqual.setForeground(overrallClr);
+
+        setBtnListeners();
+    }
+
+    private void setBtnListeners() {
+        btnZero.addActionListener(e -> {
+            String initial = this.display.getText();
+
+            if(initial.equals("0")) {
+                display.setText("0");
+            } else {
+                display.setText(initial + "0");
+            }
+
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnOne.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("1");
+            } else {
+                this.display.setText(txt + "1");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnTwo.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("2");
+            } else {
+                this.display.setText(txt + "2");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnThree.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("3");
+            } else {
+                this.display.setText(txt + "3");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnFour.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("4");
+            } else {
+                this.display.setText(txt + "4");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnFive.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("5");
+            } else {
+                this.display.setText(txt + "5");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnSix.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("6");
+            } else {
+                this.display.setText(txt + "6");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnSeven.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("7");
+            } else {
+                this.display.setText(txt + "7");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnEight.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("8");
+            } else {
+                this.display.setText(txt + "8");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnNine.addActionListener(e -> {
+            String txt = this.display.getText();
+            if(txt.equals("0")) {
+                display.setText("9");
+            } else {
+                this.display.setText(txt + "9");
+            }
+            String num1 = this.display.getText();
+            num2 = Float.parseFloat(num1);
+        });
+
+        btnPlus.addActionListener(e -> {
+            try{
+                String num1 = this.display.getText();
+
+                if (Float.parseFloat(num1.substring(num1.length() - 1)) != 0) {
+                    display.setText(num1 + "+");
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        btnMinus.addActionListener(e -> {
+            try{
+                String num1 = this.display.getText();
+
+                if (Float.parseFloat(num1.substring(num1.length() - 1)) != 0) {
+                    display.setText(num1 + "-");
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        btnMultiply.addActionListener(e -> {
+            try{
+                String num1 = this.display.getText();
+
+                if (Float.parseFloat(num1.substring(num1.length() - 1)) != 0) {
+                    display.setText(num1 + "*");
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        btnDivide.addActionListener(e -> {
+            try{
+                String num1 = this.display.getText();
+
+                if (Float.parseFloat(num1.substring(num1.length() - 1)) != 0) {
+                    display.setText(num1 + "/");
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        btnModulus.addActionListener(e -> {
+            try{
+                String num1 = this.display.getText();
+
+                if (Float.parseFloat(num1.substring(num1.length() - 1)) != 0) {
+                    display.setText(num1 + "%");
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        btnAC.addActionListener(e -> {
+            this.display.setText("0");
+        });
+
+        btnDecimal.addActionListener(e -> {
+            try{
+                String num1 = this.display.getText();
+
+                if (Float.parseFloat(num1) == num2) {
+                    display.setText(num1 + ".");
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        btnPlusMinus.addActionListener(e -> {
+            try{
+                String num1 = this.display.getText();
+
+                float num = Float.parseFloat(num1);
+
+                num *= -1;
+
+                this.display.setText(String.valueOf(num));
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
+
+        btnEqual.addActionListener(e -> {
+            String txt = this.display.getText();
+            String result = executeMathStatement(txt);
+            display.setText(result);
+        });
+
+    }
+
+    public String executeMathStatement(String str) {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        try {
+            return String.valueOf(engine.eval(str));
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    public String removeLastStr(String str) {
+        if (str != null && str.length() > 0) {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
     }
 }
